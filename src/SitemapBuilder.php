ebc56fc6db396963ff8a2dd65b51146d85ac9b13
Matthew Setter <matthew@matthewsetter.com>
2017-08-02 14:25:01 +0200

    Create a SitemapBuilder
    This class will recursively read in all the files of a given extension from a
    directory downward and from that information, if there is any, build a
    valid sitemap.xml file.

---
+++ src/SitemapBuilder.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace OwnCloud\SiteMapBuilder;
+
+use OwnCloud\SiteMapBuilder\Iterator\RestructuredTextFilterIterator;
+use Refinery29\Sitemap\Component;
+use Refinery29\Sitemap\Writer;
+
+/**
+ * Class SitemapBuilder
+ * @package OwnCloud\SiteMapBuilder
+ */
+class SitemapBuilder
+{
+    const BASE_PATH = 'https://doc.owncloud.com/server';
+    const VERSION = '10.0';
+
+    /**
+     * @var string
+     */
+    private $basePath;
+
+    /**
+     * @var string
+     */
+    private $version;
+
+    /**
+     * SitemapBuilder constructor.
+     * @param string $basePath
+     * @param string $version
+     */
+    public function __construct($basePath = self::BASE_PATH, $version = self::VERSION)
+    {
+        $this->basePath = $basePath;
+        $this->version = $version;
+    }
+
+    /**
+     * Build a list of URLs from the files of the required filetype retrieved
+     * @param string $path
+     * @param string $extension
+     * @return array
+     */
+    public function getFileList($path, $extension)
+    {
+        // retrieve a filtered list of file names, based on the specified extension.
+        $iterator = new RestructuredTextFilterIterator(new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator($path, \FilesystemIterator::CURRENT_AS_PATHNAME)
+        ), $extension);
+        $files = [];
+
+        foreach($iterator as $file) {
+            $filename = str_replace(
+                ['\\', '.rst'],
+                ['/', '.html'],
+                preg_replace(
+                    '/.*(?=user_manual|admin_manual|developer_manual)/',
+                    '',
+                    $file
+                )
+            );
+            $files[] = sprintf('%s/%s/%s', $this->basePath, $this->version, $filename);
+        }
+
+        return $files;
+    }
+
+    /**
+     * Generated a valid sitemap.xml string from the list of files supplied
+     * @param array $fileList
+     * @return string
+     */
+    public function generateSiteMapXml($fileList)
+    {
+        $urlList = [];
+
+        foreach ($fileList as $file) {
+            $url = new Component\Url($file);
+            $urlList[] = $url
+                ->withLastModified(new \DateTime())
+                ->withChangeFrequency(Component\Url::CHANGE_FREQUENCY_WEEKLY)
+                ->withPriority(0.8)
+            ;
+        }
+
+        $urlSetWriter = new Writer\UrlSetWriter();
+
+        return $urlSetWriter->write(new Component\UrlSet($urlList));
+    }
+}
---
+++ test/Iterator/SitemapBuilderTest.php
@@ -0,0 +1,176 @@
+<?php
+
+namespace OwnCloud\SiteMapBuilderTest;
+
+use org\bovigo\vfs\vfsStream;
+use org\bovigo\vfs\vfsStreamDirectory;
+use OwnCloud\SiteMapBuilder\SitemapBuilder;
+
+/**
+ * Class SitemapBuilderTest
+ * @package OwnCloud\SiteMapBuilderTest
+ */
+class SitemapBuilderTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @var array|\Traversable
+     */
+    private $fileList;
+
+    /**
+     * @var vfsStreamDirectory
+     */
+    private $root;
+
+    public function setUp()
+    {
+        $this->fileList = [
+            'ownCloud' => [
+                'documentation' => [
+                    'user_manual' => [
+                        'whats_new.rst' => 'content',
+                        'files' => [
+                            'access_webdav.rst' => 'content',
+                            'deleted_file_management.rst' => 'content',
+                            'desktop_mobile_sync.rst' => 'content',
+                            'encrypting_files.rst' => 'content',
+                            'federated_cloud_sharing.rst' => 'content',
+                            'gallery_app.rst' => 'content',
+                            'index.rst' => 'content',
+                            'large_file_upload.rst' => 'content',
+                            'public_link_shares.rst' => 'content',
+                            'troubleshooting.rst' => 'content',
+                            'version_control.rst' => 'content',
+                            'version_control.png' => 'content',
+                            'version_control.jpg' => 'content',
+                        ],
+                        'images' => [
+                            'an_image.png' => 'content',
+                            'another_image.jpg' => 'content',
+                        ]
+                    ]
+                ]
+            ]
+
+        ];
+        $this->root = vfsStream::setup('root', 755, $this->fileList);
+    }
+
+    public function testCanBuildFileListSuccessfuly()
+    {
+        $builder = new SitemapBuilder();
+        $matchingFiles = [
+            'https://doc.owncloud.com/server/10.0/user_manual/whats_new.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/access_webdav.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/deleted_file_management.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/desktop_mobile_sync.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/encrypting_files.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/federated_cloud_sharing.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/gallery_app.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/index.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/large_file_upload.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/public_link_shares.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/troubleshooting.html',
+            'https://doc.owncloud.com/server/10.0/user_manual/files/version_control.html',
+        ];
+
+        $files = $builder->getFileList(vfsStream::url('root/ownCloud/documentation'), 'rst');
+        $this->assertSame($matchingFiles, $files, "Expected a different set of matching files");
+    }
+
+    public function testCanBuildSitemapCorrectly()
+    {
+        $builder = new SitemapBuilder();
+
+        $testSitemapXml = '<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/whats_new.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/access_webdav.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/deleted_file_management.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/desktop_mobile_sync.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/encrypting_files.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/federated_cloud_sharing.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/gallery_app.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/index.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/large_file_upload.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/public_link_shares.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/troubleshooting.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+  <url>
+    <loc>https://doc.owncloud.com/server/10.0/user_manual/files/version_control.html</loc>
+    <lastmod>%1$s</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>';
+
+        $generatedSitemapXml = $builder->generateSiteMapXml(
+            $builder->getFileList(vfsStream::url('root/ownCloud/documentation'), 'rst')
+        );
+
+        // Format the generated output so that it's easier to test against
+        $dom = new \DOMDocument;
+        $dom->preserveWhiteSpace = false;
+        $dom->loadXML($generatedSitemapXml);
+        $dom->formatOutput = true;
+
+        $this->assertSame(
+            sprintf($testSitemapXml, (new \DateTime())->format(\DateTime::W3C)),
+            trim($dom->saveXml())
+        );
+    }
+}
